4dad6553435310a4fbd0fb907c2e9104a872aaf7
Kazi Rakib <kmrakib168@gmail.com>
2020-09-19 22:52:34 +0600

    Add ArrayAdapter to NumbersActivity v0.01

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,23 @@
+package com.example.android.miwok;
+
+public class Word {
+    // member variables
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    // getter and setter
+
+
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+}
