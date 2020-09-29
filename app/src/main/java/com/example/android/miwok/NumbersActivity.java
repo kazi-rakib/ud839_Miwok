ea99e15e6860a44a85fb863f570b664b0a23d7e6
Kazi Rakib <kmrakib168@gmail.com>
2020-09-30 01:29:03 +0600

    Refactor

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,6 +18,7 @@ package com.example.android.miwok;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -28,6 +29,13 @@ public class NumbersActivity extends AppCompatActivity {
 
     private MediaPlayer mMediaPlayer;
 
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,10 +44,10 @@ public class NumbersActivity extends AppCompatActivity {
         // Create a list of words
         final ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add(new Word("one", "lutti", R.raw.number_one , R.drawable.number_one));
-        words.add(new Word("two", "otiiko", R.raw.number_two , R.drawable.number_two));
+        words.add(new Word("one", "lutti", R.raw.number_one, R.drawable.number_one));
+        words.add(new Word("two", "otiiko", R.raw.number_two, R.drawable.number_two));
         words.add(new Word("three", "tolookosu", R.raw.number_three, R.drawable.number_three));
-        words.add(new Word("four", "oyyisa",  R.raw.number_four, R.drawable.number_four));
+        words.add(new Word("four", "oyyisa", R.raw.number_four, R.drawable.number_four));
         words.add(new Word("five", "massokka", R.raw.number_five, R.drawable.number_five));
         words.add(new Word("six", "temmokka", R.raw.number_six, R.drawable.number_six));
         words.add(new Word("seven", "kenekaku", R.raw.number_seven, R.drawable.number_seven));
@@ -72,9 +80,37 @@ public class NumbersActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 // i == position ?
                 Word word = words.get(i);
+//                Log.i("NumberActivity", "Current word: " + word);
+                releaseMediaPlayer();
                 mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudioResourceID());
                 mMediaPlayer.start();
+                mMediaPlayer.setOnCompletionListener(mCompletionListener);
             }
         });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // When the activity is stopped, release the media player resources because we won't
+        // be playing any more sounds.
+        releaseMediaPlayer();
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+        }
     }
 }
